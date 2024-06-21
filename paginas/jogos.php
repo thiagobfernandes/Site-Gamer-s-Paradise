34e395df489080ea89daccc6c03a069ef03ef023
thiagobfernandes <thiago5437br@gmail.com>
2024-06-21 13:16:55 -0300

    pagina de jogos

--- paginas/jogos.php
+++ paginas/jogos.php
@@ -5,10 +5,10 @@ $dados= json_decode($dados);
 $dados= $dados -> $codigo;
 
     ?>
-    <section class="container">
-        <div class="jogoszada text-center">
-            <img src="<?=$base_url?>/<?=$dados ->banner?>" alt="">
-            <a href="<?=$dados -> jogo ?>" class="btn btn-success mb-2 w-25 fs-4 rounded-5"> Jogar agora  <i class="fa-solid fa-play"></i></a>
+    <section class="container p-4 text-center w-100">
+        <div class="jogoszada text-center d-none d-md-block">
+            <a data-fslightbox href="<?=$base_url?>/<?=$dados ->banner?>"> <img src="<?=$base_url?>/<?=$dados ->banner?>" alt=""></a>
+            <a href="<?=$dados -> jogo ?>" target="_blank" class="btn btn-success mb-2 w-50 fs-4 rounded-5 my-4 "> Jogar agora  <i class="fa-solid fa-play"></i></a>
         
             </div>
 
@@ -19,14 +19,14 @@ $dados= $dados -> $codigo;
 </div>
 
 
-            <div class="row w-100 mt-4">
+            <div class="row w-100 text-center  ">
 
-            <div class="imagem col-12 col-md-3 col-lg-4 ">
-                <img src="<?=$base_url?>/<?=$dados -> banner?>" alt="<?=$dados -> nome?>" class="w-100">
+            <div class="col-12 col-md-3 col-lg-4 text-center ">
+                <a  data-fslightbox href="<?=$base_url?>/<?=$dados -> capa?>"><img src="<?=$base_url?>/<?=$dados -> capa?>" alt="<?=$dados -> nome?>"class="w-100 m-3"></a>
             </div>
-            <div class="texto col-12 col-md-9 col-lg-8 text-light text-center ">
+            <div class="texto col-12 col-md-9 mt-3 col-lg-8 text-light text-center ">
                 <h2><?=$dados -> nome ?></h2>
-                <p class="fs-5"><?=$dados -> descricao?></p>
+                <p class="fs-3"><?=$dados -> descricao?></p>
 
 
             </div>
